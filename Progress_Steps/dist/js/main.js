2cc8875febb86b32bcc7df06e2770a8f254c3b7a
Angelos Barmpoutis <angelosbar97@gmail.com>
2022-05-30 15:59:00 +0300

    Change progress' bar formula
    I figured out that my working formula was a work-around so I decided to make it more scalable and applicable to more applications with different ammount of steps. In this particular project there are 4 steps in total but only 3 remaining since step 1 is already marked as active. So our step cant go up or down by 25%.By substracting 1 from each side I accomplished to change the proportion to the maximum of 3. For example If we are on step 1 and click the next button the progress bar will move by 1/3 of its total, 33%.

--- Progress_Steps/dist/js/main.js
+++ Progress_Steps/dist/js/main.js
@@ -6,19 +6,15 @@ const circles = document.querySelectorAll('.circle');
 
 // Declare Variables
 let level = 1;
-const levels =
-{
-    1: '0%',
-    2: '37%',
-    3: '68%',
-    4: '100%'
-}
+
 
 // Set Event Listeners
 btnNext.addEventListener('click', e => {
     level++;
-    progressBar.style.width = `${levels[level]}`;
     circles[level-1].classList.add('active');
+    const actives = document.querySelectorAll('.active');
+    // There are 4 steps in total but only 3 remaining since step 1 is already marked as active. So our step cant go up or down by 25%.By substracting 1 from each side I accomplished to change the proportion to the maximum of 3. For example If we are on step 1 and click the next button the progress bar will move by 1/3 of its total, 33%.
+    progressBar.style.width = `${(actives.length -1) / (circles.length - 1) * 100}%`;
     btnPrevious.removeAttribute('disabled');
     if (level === 4) {
         e.target.setAttribute('disabled','');
@@ -28,10 +24,10 @@ btnNext.addEventListener('click', e => {
 })
 
 btnPrevious.addEventListener('click', e => {
-    console.log(level);
     level--
-    progressBar.style.width = `${levels[level]}`;
     circles[level].classList.remove('active');
+    const actives = document.querySelectorAll('.active');
+    progressBar.style.width = `${(actives.length -1) / (circles.length - 1) * 100}%`;
     btnNext.removeAttribute('disabled');
     if (level === 1) {
         e.target.setAttribute('disabled','');
